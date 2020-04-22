08dc456d310c5dde33a078e978521292dea843dd
zce <w@zce.me>
2020-04-22 14:37:50 +0800

    chore: update router

--- 04-official-libraries/src/router/index.js
+++ 04-official-libraries/src/router/index.js
@@ -1,25 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about/:slug?',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = createRouter({
+export default createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/about/:slug?',
+      name: 'About',
+      component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    }
+  ]
 })
-
-export default router
